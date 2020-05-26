e5328a37c0e01838da6ff7a126fe30d4a9ce28ac
Patrick Gabbard <gabbbarddesigns@gmail.com>
2020-05-26 12:50:27 -0400

    Changed menu structure, added main menu and broke out Add menu fucntion.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Grovery List
 ### A simple Python script to allow the user to build and view a grocery list.
-
--- grovery_list.py
+++ grovery_list.py
@@ -1,15 +1,36 @@
 shopping_list=[]
 plural="item"
 
-def show_help():
+def main_menu(plural):
+    while True:
+        print("Please select an option from the menu.")
+        print("""
+        Enter 'ADD' to add items to your shopping list
+        ENTER 'SHOW' to view the list
+        ENTER 'QUIT' to end the program.""")
+        menu_selection = input("> ").upper()
+        if menu_selection == 'QUIT':
+            break
+        elif menu_selection == 'ADD':
+            add_items(plural)
+            continue
+        elif menu_selection == 'SHOW':
+            show_list()
+            input("""
+            Press any key to continue.""") 
+            continue
+
+
+
+def show_menu():
     print("What should we pick up at the store?")
     print("""
     Enter 'DONE' to stop adding items
     Enter 'HELP' to get help
     ENTER 'SHOW' to view the list.""")
 
-
 def add_items(plural):
+    show_menu()
     while True:
         new_item = input("> ")
         if len(shopping_list)>0:
@@ -17,10 +38,11 @@ def add_items(plural):
         if new_item.upper() == 'DONE':
             break
         elif new_item.upper() == 'HELP':
-            show_help()
+            show_menu()
             continue
         elif new_item.upper() == 'SHOW':
             show_list()
+            show_menu()
             continue
         else:
             shopping_list.append(new_item)
@@ -30,10 +52,9 @@ def add_items(plural):
 def show_list():
     print("Your shopping list:")
     for items in shopping_list:
-        print(items)            
+        print(items)           
 
-show_help()
-add_items(plural)
+main_menu(plural)
 
 
 
